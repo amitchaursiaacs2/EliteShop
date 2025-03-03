38e5da62597ca80be98b65af470ec81ecf6ace27
Chaurasia Amit <amitchaurasiaacs@gmail.com>
2025-03-04 00:45:52 +0530

    Initial commit

---
+++ src/components/SearchDropdown/Dropdown.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Dropdown.css'
+
+const Dropdown = () => {
+    return (
+        <>
+            <div className="selectDrops">
+                <div className="searchField">
+                    <input type="text" />
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Dropdown
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Header.css";
+import { FaSearch } from "react-icons/fa";
+import Dropdown from "../SearchDropdown/Dropdown";
+
+const Header = () => {
+    return (
+        <>
+            <header>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-sm-2">
+                            <img src="/assets/eliteshop.svg" alt="" />
+                        </div>
+                        <div className="col-sm-5">
+                            <div className="headerSearch d-flex align-items-center">
+                                <div className="selectDrop cursor position-relative">
+                                    All Categories
+                                    <Dropdown />
+                                </div>
+                                <div className="search">
+                                    <input type="text" placeholder="Search for item" />
+                                    <FaSearch className="searchIcon cursor" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </>
+    );
+};
+
+export default Header;
