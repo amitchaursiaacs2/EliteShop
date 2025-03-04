05b7ae87b7ca8d4a85964b7491e95a142222cdef
Chaurasia Amit <amitchaurasiaacs@gmail.com>
2025-03-04 18:14:02 +0530

    Update

--- src/components/SearchDropdown/Dropdown.jsx
+++ src/components/SearchDropdown/Dropdown.jsx
@@ -1,45 +1,65 @@
 import React, { useState } from "react";
 import "./Dropdown.css";
+import { IoIosArrowDown } from "react-icons/io";
+import { ClickAwayListener } from "@mui/material";
 
-const Dropdown = () => {
+const Dropdown = ({ data, placeholder, icon }) => {
     const [open, setOpen] = useState(false);
     const [selected, setSelected] = useState(0);
+    const [selectedItem, setSelectedItem] = useState(placeholder);
+
+    const [listData, setListData] = useState(data)
+    const [listCountryData, setListCountryData] = useState(data)
 
     const openDrop = () => {
         setOpen(!open);
     }
 
-    const closeDrop = (index) => {
+    const closeDrop = (index, name) => {
         setSelected(index);
         setOpen(false);
+        setSelectedItem(name);
+    }
+
+    // Data filter funtion
+    const filterList = (e) => {
+        const keyword = e.target.value.toLowerCase();
+        const list = listCountryData.filter((item) => {
+            return item.toLowerCase().includes(keyword);
+        })
+        // For dublicate data
+        const list2 = list.filter((item, index) => list.indexOf(item) === index)
+
+        setListData(list2)
+
     }
 
     return (
         <>
-            <div className="selectDropWrapper cursor position-relative">
-                <span className="openSelect" onClick={openDrop}>All Categories</span>
-                {
-                    open === true &&
-                    <div className="selectDrops">
-                        <div className="searchField">
-                            <input type="text" />
+            <ClickAwayListener onClickAway={() => setOpen(false)}>
+                <div className="selectDropWrapper cursor position-relative">
+                    {icon}
+                    <span className="openSelect" onClick={openDrop}>{selectedItem.length > 10 ? selectedItem.substr(0, 10) + "..." : selectedItem}<IoIosArrowDown className="arrowDrop" /> </span>
+                    {
+                        open === true &&
+                        <div className="selectDrops">
+                            <div className="searchField">
+                                <input type="text" placeholder="Search here..." onChange={filterList} />
+                            </div>
+                            <ul className="searctResults">
+                                <li key={0} onClick={() => closeDrop(0, placeholder)} className={`${selected === placeholder ? 'active' : ''}`}>{placeholder}</li>
+                                {
+                                    listData.map((item, index) => {
+                                        return (
+                                            <li key={index} onClick={() => closeDrop(index, item)} className={`${selected === index ? 'active' : ''}`}>{item}</li>
+                                        )
+                                    })
+                                }
+                            </ul>
                         </div>
-                        <ul className="searctResults">
-                            <li onClick={() => closeDrop(0)} className={`${selected === 0 ? 'active' : ''}`}>All Categories</li>
-                            <li onClick={() => closeDrop(1)} className={`${selected === 1 ? 'active' : ''}`}>Milks & Daairies</li>
-                            <li onClick={() => closeDrop(2)} className={`${selected === 2 ? 'active' : ''}`}>Wines & Drinks</li>
-                            <li onClick={() => closeDrop(3)} className={`${selected === 3 ? 'active' : ''}`}>Clothing & Beauty</li>
-                            <li onClick={() => closeDrop(4)} className={`${selected === 4 ? 'active' : ''}`}>Fresh Food & Toy</li>
-                            <li onClick={() => closeDrop(5)} className={`${selected === 5 ? 'active' : ''}`}>Fast Food</li>
-                            <li onClick={() => closeDrop(6)} className={`${selected === 6 ? 'active' : ''}`}>Baking Meterial</li>
-                            <li onClick={() => closeDrop(7)} className={`${selected === 7 ? 'active' : ''}`}>Vegetables</li>
-                            <li onClick={() => closeDrop(8)} className={`${selected === 8 ? 'active' : ''}`}>Fresh Fruits</li>
-                            <li onClick={() => closeDrop(9)} className={`${selected === 9 ? 'active' : ''}`}>Bread and Juice</li>
-                            <li onClick={() => closeDrop(10)} className={`${selected === 10 ? 'active' : ''}`}>Fresh Seafood</li>
-                        </ul>
-                    </div>
-                }
-            </div>
+                    }
+                </div>
+            </ClickAwayListener>
         </>
     );
 };
