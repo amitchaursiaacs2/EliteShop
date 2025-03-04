2218024d9823b7bec987e0ddfddd9d1fc766e5f9
Chaurasia Amit <amitchaurasiaacs@gmail.com>
2025-03-04 11:28:39 +0530

    Update

--- src/components/SearchDropdown/Dropdown.jsx
+++ src/components/SearchDropdown/Dropdown.jsx
@@ -1,38 +1,47 @@
-import React from 'react'
-import './Dropdown.css'
+import React, { useState } from "react";
+import "./Dropdown.css";
 
 const Dropdown = () => {
+    const [open, setOpen] = useState(false);
+    const [selected, setSelected] = useState(0);
+
+    const openDrop = () => {
+        setOpen(!open);
+    }
+
+    const closeDrop = (index) => {
+        setSelected(index);
+        setOpen(false);
+    }
+
     return (
         <>
-            <div className="selectDrops">
-                <div className="searchField">
-                    <input type="text" />
-                    <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                </div>
+            <div className="selectDropWrapper cursor position-relative">
+                <span className="openSelect" onClick={openDrop}>All Categories</span>
+                {
+                    open === true &&
+                    <div className="selectDrops">
+                        <div className="searchField">
+                            <input type="text" />
+                        </div>
+                        <ul className="searctResults">
+                            <li onClick={() => closeDrop(0)} className={`${selected === 0 ? 'active' : ''}`}>All Categories</li>
+                            <li onClick={() => closeDrop(1)} className={`${selected === 1 ? 'active' : ''}`}>Milks & Daairies</li>
+                            <li onClick={() => closeDrop(2)} className={`${selected === 2 ? 'active' : ''}`}>Wines & Drinks</li>
+                            <li onClick={() => closeDrop(3)} className={`${selected === 3 ? 'active' : ''}`}>Clothing & Beauty</li>
+                            <li onClick={() => closeDrop(4)} className={`${selected === 4 ? 'active' : ''}`}>Fresh Food & Toy</li>
+                            <li onClick={() => closeDrop(5)} className={`${selected === 5 ? 'active' : ''}`}>Fast Food</li>
+                            <li onClick={() => closeDrop(6)} className={`${selected === 6 ? 'active' : ''}`}>Baking Meterial</li>
+                            <li onClick={() => closeDrop(7)} className={`${selected === 7 ? 'active' : ''}`}>Vegetables</li>
+                            <li onClick={() => closeDrop(8)} className={`${selected === 8 ? 'active' : ''}`}>Fresh Fruits</li>
+                            <li onClick={() => closeDrop(9)} className={`${selected === 9 ? 'active' : ''}`}>Bread and Juice</li>
+                            <li onClick={() => closeDrop(10)} className={`${selected === 10 ? 'active' : ''}`}>Fresh Seafood</li>
+                        </ul>
+                    </div>
+                }
             </div>
         </>
-    )
-}
+    );
+};
 
-export default Dropdown
+export default Dropdown;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,31 +4,28 @@ import { FaSearch } from "react-icons/fa";
 import Dropdown from "../SearchDropdown/Dropdown";
 
 const Header = () => {
-    return (
-        <>
-            <header>
-                <div className="container-fluid">
-                    <div className="row">
-                        <div className="col-sm-2">
-                            <img src="/assets/eliteshop.svg" alt="" />
-                        </div>
-                        <div className="col-sm-5">
-                            <div className="headerSearch d-flex align-items-center">
-                                <div className="selectDrop cursor position-relative">
-                                    All Categories
-                                    <Dropdown />
-                                </div>
-                                <div className="search">
-                                    <input type="text" placeholder="Search for item" />
-                                    <FaSearch className="searchIcon cursor" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <>
+      <header>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-sm-2">
+              <img src="/assets/eliteshop.svg" alt="" />
+            </div>
+            <div className="col-sm-5">
+              <div className="headerSearch d-flex align-items-center">
+                <Dropdown />
+                <div className="search">
+                  <input type="text" placeholder="Search for item" />
+                  <FaSearch className="searchIcon cursor" />
                 </div>
-            </header>
-        </>
-    );
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Header;
